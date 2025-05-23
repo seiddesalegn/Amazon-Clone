e354f865572d46dce76e88d92390cb6ab967258f
seiddesalegn <seiddesalegn4@gmail.com>
2025-05-22 21:38:01 -0700

    product-categories-image

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import Hero from "../Components/Hero/Hero";
-import ProductRow from "../components/ProductRow/ProductRow";
+import ProductCategories from "../Components/ProductCategories/ProductCategories";
 import styles from "./Home.module.css";
 
 const Home = () => {
   return (
     <div className={styles.home}>
       <Hero />
-      <ProductRow title="Best Sellers" />
-      <ProductRow title="New Arrivals" />
+      <ProductCategories />
     </div>
   );
 };
