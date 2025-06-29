386b86379899a98d58609cfc9994b36587120503
seiddesalegn <seiddesalegn4@gmail.com>
2025-06-28 21:29:31 -0700

    Fix: corrected Hero import path for Netlify

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Hero from "../components/Hero/Hero";
+import Hero from "../Components/Hero/Hero";
 import Category from "../components/Category/Category";
 import Product from "../components/Product/Product";
 
