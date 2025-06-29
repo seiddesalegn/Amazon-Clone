2a15348d01f32acf5f71bb645ea30a606217e547
seiddesalegn <seiddesalegn4@gmail.com>
2025-06-28 22:36:36 -0700

    Fix: corrected CSS module path in Category component

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { img } from "./img/data"; //used this insted fullinfos
 import CategoryCard from "./CategoryCard";
-import classes from "./category.module.css";
+import classes from "./Category.module.css";
 
 function Category() {
   return (
